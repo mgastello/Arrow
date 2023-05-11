9e2adb199a5c14ae636766230669cf5f055b7540
Michael Gastello <michaelgastello@aol.com>
2023-05-10 20:26:28 -0400

    create reviews controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,4 +1,49 @@
 class Api::ReviewsController < ApplicationController
     wrap_parameters include: CartItem.attribute_names + ['userId', 'productId', 'displayName']
+    before_action :require_logged_in
 
+    def index
+        @reviews = Product.find(params[:product_id]).reviews
+        render :index
+    end
+
+    def show
+        @review = Review.find(params[:id])
+        render :show
+    end
+
+    def create
+        @review = Review.new(review_params)
+
+        if @review.save
+            render :show
+        else
+            render json: { errors: @review.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @review = Review.find(params[:id])
+
+        if @review.update(review_params)
+            render :show
+        else
+            render json: { errors: @review.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @review = Review.find(params[:id])
+        if @review.destroy
+            render json: { message: 'successfully deleted' }
+        else
+            render json: { message: 'unable to delete review' }
+        end
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:author_id, :product_id, :title, :body, :rating, :display_name)
+    end
 end
