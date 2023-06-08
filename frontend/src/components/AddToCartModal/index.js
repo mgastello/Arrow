87f695028540aec6b29094db79ae33497e8c4b25
Michael Gastello <michaelgastello@aol.com>
2023-06-08 05:56:52 -0400

    fixed broken add to cart by commenting out stars on product index

--- frontend/src/components/AddToCartModal/index.js
+++ frontend/src/components/AddToCartModal/index.js
@@ -4,7 +4,7 @@ import { CartModal } from '../../context/CartModal';
 import * as cartActions from '../../store/cart'
 import { useHistory } from 'react-router-dom';
 
-function AddToCartModal({product}) {
+function AddToCartModal({ product }) {
   const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch()
   const sessionUser = useSelector(state => state.session.user);
@@ -25,14 +25,14 @@ function AddToCartModal({product}) {
 
   return (
     <>
-        <button id="add-to-cart-button" onClick={addToCart}>Add to cart</button>
+      <button id="add-to-cart-button" onClick={addToCart}>Add to cart</button>
       {showModal && (
         <CartModal onClose={() => setShowModal(false)}>
-            <h3 className="cart-modal-items" id='2day-text'>2-day shipping</h3>
-            <div id='a2c-product'>
-                <img className="cart-modal-items" id='cart-pic' src={product.pictureUrl} alt='' />
-                <h4 className="cart-modal-items" id='cart-name'>{product.name}</h4>
-            </div>
+          <h3 className="cart-modal-items" id='2day-text'>2-day shipping</h3>
+          <div id='a2c-product'>
+            <img className="cart-modal-items" id='cart-pic' src={product.pictureUrl} alt='' />
+            <h4 className="cart-modal-items" id='cart-name'>{product.name}</h4>
+          </div>
         </CartModal>
       )}
     </>
--- frontend/src/components/Products/ProductsIndex/ProductIndexItem.js
+++ frontend/src/components/Products/ProductsIndex/ProductIndexItem.js
@@ -1,33 +1,33 @@
 import React, { useEffect } from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import AddToCartModal from "../../AddToCartModal";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchAllReviews } from "../../../store/review";
-import StarRatings from "react-star-ratings";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchAllReviews } from "../../../store/review";
+// import StarRatings from "react-star-ratings";
 
 export default function ProductIndexItem({ product }) {
-    const dispatch = useDispatch()
-    const reviews = useSelector(state => Object.values(state?.reviews))
+    // const dispatch = useDispatch()
+    // const reviews = useSelector(state => Object.values(state?.reviews))
 
-    useEffect(() => {
-        dispatch(fetchAllReviews())
-    }, [dispatch])
+    // useEffect(() => {
+    //     dispatch(fetchAllReviews())
+    // }, [dispatch])
 
-    let sumRating = 0
-    let numReviews = 0
+    // let sumRating = 0
+    // let numReviews = 0
 
-    if (reviews) {
-        reviews.forEach((review) => {
-            if (review && product) {
-                if (review.productId === product.id) {
-                    sumRating += review.rating;
-                    numReviews += 1;
-                }
-            }
-        })
-    }
+    // if (reviews) {
+    //     reviews.forEach((review) => {
+    //         if (review && product) {
+    //             if (review.productId === product.id) {
+    //                 sumRating += review.rating;
+    //                 numReviews += 1;
+    //             }
+    //         }
+    //     })
+    // }
 
-    const avgRating = numReviews === 0 ? 0 : sumRating / numReviews;
+    // const avgRating = numReviews === 0 ? 0 : sumRating / numReviews;
 
     return (
         <>
@@ -37,13 +37,13 @@ export default function ProductIndexItem({ product }) {
                     <h3 id="product-name">{product.name}</h3>
                 </Link>
                 <p id="product-brand">{product.brand}</p>
-                <StarRatings
+                {/* <StarRatings
                     rating={avgRating}
                     starRatedColor="gold"
                     starEmptyColor="lightgray"
                     starDimension="15px"
                     starSpacing="0px"
-                />
+                /> */}
                 <p id="product-price">${product.price.toFixed(2)}</p>
                 <p id="purchase-online-text-card">When purchased online</p>
                 <p id="free-shipping-text">Free Shipping*</p>
