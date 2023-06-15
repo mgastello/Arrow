9ce49c538aa384bb51f32fd3b22debc70f407ed6
Michael Gastello <michaelgastello@aol.com>
2023-06-14 21:55:23 -0400

    shipping adress box done

--- frontend/src/components/Checkout/CheckoutPage.js
+++ frontend/src/components/Checkout/CheckoutPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from "react-router-dom"
 import { fetchCartItems } from '../../store/cart';
+import moment from 'moment';
 import "./CheckoutPage.css"
 
 export default function CheckoutPage() {
@@ -14,6 +15,12 @@ export default function CheckoutPage() {
     const taxPrice = parseFloat((subtotalNumber * .08625).toFixed(2))
     const total = (taxPrice + subtotalNumber)
     const [loading, setLoading] = useState(true)
+
+    const getArrivalDate = () => {
+        const currentDate = moment();
+        const arrivalDate = currentDate.add(5, 'days');
+        return arrivalDate.format('ddd, MMM D');
+    };
 
     useEffect(() => {
         dispatch(fetchCartItems())
@@ -33,7 +40,6 @@ export default function CheckoutPage() {
             <Redirect to="/login" />
         )
     } else if (cartItems.length === 0) {
-        debugger
         return (
             <Redirect to="/cart" />
         )
@@ -44,29 +50,75 @@ export default function CheckoutPage() {
                     <div className='checkout-items'>
                         <div className='all-cart-items-container'>
                             <ul id='all-cart-items'>
-                                <div className='checkout-items-header'>
-                                    <div>
-                                        <img className="arrow-box-img" src='https://gcdnb.pbrd.co/images/GCSOCYuiVRr6.png?o=1' alt='arrow-shipping-box' />
-                                    </div>
-                                    <div className='checkout-items-header-words'>
-                                        <h1 className='checkout-items-header-text'>Cart</h1>
-                                        <h2 className='checkout-items-subtotal-text'>${subtotalNumber.toFixed(2)} subtotal • {totalItems} items</h2>
+                                <div className='checkout-items-wrapper'>
+                                    <div className='checkout-items-header'>
+                                        <div>
+                                            <img className="arrow-box-img" src='https://gcdnb.pbrd.co/images/9VkZtX9K1TRb.png?o=1' alt='shopping-cart' />
+                                        </div>
+                                        <div className='checkout-items-header-words'>
+                                            <div>
+                                                <h1 className='checkout-items-header-text'>Cart</h1>
+                                                <h2 className='checkout-items-subtotal-text'>${subtotalNumber.toFixed(2)} subtotal • {totalItems} items</h2>
+                                            </div>
+                                            <div className="checkout-page-edit-button">
+                                                <a className='checkout-page-edit-button-word' href='/cart'>Edit</a>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <h3 id='holder-header-shipping'>Shipping</h3>
-                                <p id='item-count'>{totalItems} items</p>
-                                {cartItems.map((cartItem) => {
-                                    return (
-                                        <img className="checkout-page-item-imgs" src={cartItem.pictureUrl} alt="item-pic" />
-                                    )
-                                })}
+                                <h3 id='checkout-arrives-by'>Arrives by {getArrivalDate()}</h3>
+                                <div className='checkout-page-imgs-wrapper'>
+                                    {cartItems.map((cartItem) => {
+                                        return (
+                                            <img className="checkout-page-item-imgs" src={cartItem.pictureUrl} alt="item-pic" />
+                                        )
+                                    })}
+                                </div>
                             </ul>
                         </div>
                         <div className='checkout-user-info'>
+                            <ul id='all-cart-items'>
+                                <div className='checkout-items-wrapper'>
+                                    <div className='checkout-items-header'>
+                                        <div>
+                                            <img className="arrow-box-img" src='https://gcdnb.pbrd.co/images/lyqBfWKwkkNn.png?o=1' alt='arrow-shipping-box' />
+                                        </div>
+                                        <div className='checkout-items-header-words'>
+                                            <h1 className='checkout-items-header-text'>Shipping Address</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='user-info-wrapper'>
+                                    <p className='user-info'>{sessionUser.firstName} {sessionUser.lastName}</p>
+                                    <p className='user-info'>90 5th Ave</p>
+                                    <p className='user-info'>New York, NY, 10011</p>
+                                    <p className='user-info'>845-253-5164</p>
+                                    <p className='default-address-box'>Default address</p>
+                                </div>
+                                <div className='checkout-page-imgs-wrapper'>
 
+                                </div>
+                            </ul>
                         </div>
                         <div className='checkout-payment'>
+                            <ul id='all-cart-items'>
+                                <div className='checkout-items-wrapper'>
+                                    <div className='checkout-items-header'>
+                                        <div>
+                                            <img className="arrow-box-img" src='https://gcdnb.pbrd.co/images/pkPc8qdjd1Rk.png?o=1' alt='arrow-shipping-box' />
+                                        </div>
+                                        <div className='checkout-items-header-words'>
+                                            <h1 className='checkout-items-header-text'>Payment</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='user-info-wrapper'>
+                                    <p className='user-info'>{sessionUser.firstName} {sessionUser.lastName}</p>
+                                </div>
+                                <div className='checkout-page-imgs-wrapper'>
 
+                                </div>
+                            </ul>
                         </div>
                     </div>
                 </div>
