82839a651867e269117859313460c728a69df755
Michael Gastello <michaelgastello@aol.com>
2023-07-07 15:43:37 -0400

    reworking main page

--- frontend/src/components/Products/ProductsIndex/index.js
+++ frontend/src/components/Products/ProductsIndex/index.js
@@ -1,11 +1,9 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { fetchProducts } from '../../../store/product';
-import ProductIndexItem from './ProductIndexItem';
 import './ProductIndex.css';
 
 export default function ProductIndex() {
-    const products = useSelector(state => Object.values(state?.products))
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -13,15 +11,6 @@ export default function ProductIndex() {
     }, [dispatch]);
 
     return (
-        // <>
-        //     <div className='all-products-container'>
-        //         <ul id='all-products'>
-        //             {products.map((product) => {
-        //                 return <ProductIndexItem key={product.id} product={product} />
-        //             })}
-        //         </ul>
-        //     </div>
-        // </>
         <div className='homepage'>
             <div className='top-home-page'>
                 <h1 className='top-homepage-header'>Summer Spending</h1>
