9f708a8cf05427514b141671029141a76a62e3d9
Michael Gastello <michaelgastello@aol.com>
2023-07-30 20:09:17 -0400

    working on homepage

--- frontend/src/components/Products/ProductsIndex/index.js
+++ frontend/src/components/Products/ProductsIndex/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch } from "react-redux";
 import { fetchProducts } from '../../../store/product';
 import './ProductIndex.css';
+import arrowHomePage from '../../../images/arrowHomePage.jpg'
 
 export default function ProductIndex() {
     const dispatch = useDispatch();
@@ -13,7 +14,9 @@ export default function ProductIndex() {
     return (
         <div className='homepage'>
             <div className='top-home-page'>
-                <h1 className='top-homepage-header'>Summer Spending</h1>
+                <button className='home-page-button'>
+                    <img className='home-page-img' src={arrowHomePage} alt='' />
+                </button>
             </div>
             <div className='bottom-home-page'>
                 <h1 className='featured-categories-header'>Featured categories</h1>
