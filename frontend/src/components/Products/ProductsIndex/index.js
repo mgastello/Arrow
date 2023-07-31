6eeb4f31b43a80011271f92ff2023c677aacc1dd
Michael Gastello <michaelgastello@aol.com>
2023-07-31 03:50:53 -0400

    styled top of home page

--- frontend/src/components/Products/ProductsIndex/index.js
+++ frontend/src/components/Products/ProductsIndex/index.js
@@ -15,8 +15,14 @@ export default function ProductIndex() {
         <div className='homepage'>
             <div className='top-home-page'>
                 <button className='home-page-button'>
-                    <h1 className='home-page-text'>Start school</h1>
                     <img className='home-page-img' src={arrowHomePage} alt='' />
+                    <div className='home-page-text'>
+                        <h1 className='home-page-h1'>Ace the first day</h1>
+                        <br></br>
+                        <p className='home-page-p'>Grab their gear & get it fast with always-free Shipping</p>
+                        <br></br>
+                        <button className='ready-for-school-button'>Ready for School</button>
+                    </div>
                 </button>
             </div>
             <div className='bottom-home-page'>
