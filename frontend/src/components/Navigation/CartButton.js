96528b8c9b96acdbee03eeee2abd07a9206e4a5d
Michael Gastello <michaelgastello@aol.com>
2023-08-06 15:37:27 -0400

    added cart item counter

--- frontend/src/components/Navigation/CartButton.js
+++ frontend/src/components/Navigation/CartButton.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 export default function CartButton() {
+  const cartItems = useSelector(state => Object.values(state?.cartItems))
 
   return (
     <>
         <Link to="/cart" id='cart-button-link'>
             <button id='cart-actual-button'>
                 <i className="fa-solid fa-cart-shopping" id='cart-icon'></i>
+                {cartItems.length > 0 ? (
+                  <p className='cart-item-counter'>{cartItems.length}</p>
+                ) : (
+                  null
+                )}
             </button>
         </Link>
     </>
